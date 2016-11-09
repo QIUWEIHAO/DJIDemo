3d04a49af83b2d5056e44da8620f13d45c960015
weihao.qiu <weihao.qiu@jiheup.com>
2016-11-09 00:34:10 -0800

    bad commit

---
+++ bridge.h
@@ -0,0 +1,13 @@
+//
+//  bridge.h
+//  DJISDKSwiftDemo
+//
+//  Created by Scofield on 11/3/16.
+//  Copyright © 2016 DJI. All rights reserved.
+//
+
+#ifndef bridge_h
+#define bridge_h
+
+
+#endif /* bridge_h */
